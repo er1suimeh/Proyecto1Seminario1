bd5ea00b381cabfa893fcadf0baf8785fdbdbec2
Erick <suimeh@suimeh-laptop>
2012-11-01 19:34:32 -0600

    mi commit de prueba

---
+++ HolaMundoJava/src/prueba/hola.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package prueba;
+
+/**
+ * @author suimeh
+ *
+ */
+public class hola {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo java");
+
+	}
+
+}
